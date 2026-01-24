e7721e4789f70ad9697f3e448f99cc719a22b467
kim minsu <yaminsu5674@gmail.com>
2026-01-24 19:34:58 +0900

    feat: Ability Activation Policy

---
+++ Source/Jujutsu/Private/AbilitySystem/Abilities/JujutsuGameplayAbility.cpp
@@ -0,0 +1,32 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+
+#include "AbilitySystem/Abilities/JujutsuGameplayAbility.h"
+#include "AbilitySystem/JujutsuAbilitySystemComponent.h"
+
+void UJujutsuGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	if (AbilityActivationPolicy == EJujutsuAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UJujutsuGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if (AbilityActivationPolicy == EJujutsuAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+}
---
+++ Source/Jujutsu/Public/AbilitySystem/Abilities/JujutsuGameplayAbility.h
@@ -0,0 +1,33 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "JujutsuGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class EJujutsuAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ *
+ */
+UCLASS()
+class JUJUTSU_API UJujutsuGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	//~ Begin UGameplayAbility Interface.
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec);
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled);
+	//~ End UGameplayAbility Interface
+
+	UPROPERTY(EditDefaultsOnly, Category = "JujutsuAbility")
+	EJujutsuAbilityActivationPolicy AbilityActivationPolicy = EJujutsuAbilityActivationPolicy::OnTriggered;
+
+};
