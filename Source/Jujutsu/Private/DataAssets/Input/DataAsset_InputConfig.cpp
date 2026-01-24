359f08d31fd4583597faf07212395975893f9c81
kim minsu <yaminsu5674@gmail.com>
2026-01-24 16:39:47 +0900

    feat: Input Config Data Asset

---
+++ Source/Jujutsu/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -0,0 +1,18 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+
+UInputAction* UDataAsset_InputConfig::FindNativeInputActionByTag(const FGameplayTag& InInputTag)
+{
+	for (const FJujutsuInputActionConfig& InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+
+	return nullptr;
+}
---
+++ Source/Jujutsu/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,42 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FJujutsuInputActionConfig
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+
+/**
+ *
+ */
+UCLASS()
+class JUJUTSU_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FJujutsuInputActionConfig> NativeInputActions;
+
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+};
