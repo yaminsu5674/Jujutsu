18168c7160926647564ab40e7ec746918da2b882
kim minsu <yaminsu5674@gmail.com>
2026-01-24 14:59:58 +0900

    feat: create AIController && GameModes

---
+++ Source/Jujutsu/Public/Controllers/JujutsuAIController.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "JujutsuAIController.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class JUJUTSU_API AJujutsuAIController : public AAIController
+{
+	GENERATED_BODY()
+
+};
---
+++ Source/Jujutsu/Public/GameModes/JujutsuMultiGameMode.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameModes/JujutsuBaseGameMode.h"
+#include "JujutsuMultiGameMode.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class JUJUTSU_API AJujutsuMultiGameMode : public AJujutsuBaseGameMode
+{
+	GENERATED_BODY()
+
+};
