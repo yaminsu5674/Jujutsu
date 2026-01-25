1f4902a353de1d9565380a1648cc2ea6ee55076e
kim minsu <yaminsu5674@gmail.com>
2026-01-25 21:01:25 +0900

    feat: Attribute Set

--- Source/Jujutsu/Private/AbilitySystem/JujutsuAttributeSet.cpp
+++ Source/Jujutsu/Private/AbilitySystem/JujutsuAttributeSet.cpp
@@ -1,5 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-
-
 #include "AbilitySystem/JujutsuAttributeSet.h"
+
+UJujutsuAttributeSet::UJujutsuAttributeSet()
+{
+	InitCurrentHealth(1.f);
+	InitMaxHealth(1.f);
+	InitCurrentRage(1.f);
+	InitMaxRage(1.f);
+	InitAttackPower(1.f);
+	InitDefensePower(1.f);
+}
--- Source/Jujutsu/Public/AbilitySystem/JujutsuAttributeSet.h
+++ Source/Jujutsu/Public/AbilitySystem/JujutsuAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/JujutsuAbilitySystemComponent.h"
 #include "JujutsuAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  *
@@ -14,4 +21,30 @@ class JUJUTSU_API UJujutsuAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 
+public:
+	UJujutsuAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UJujutsuAttributeSet, CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UJujutsuAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UJujutsuAttributeSet, CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UJujutsuAttributeSet, MaxRage)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UJujutsuAttributeSet, AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UJujutsuAttributeSet, DefensePower)
 };
