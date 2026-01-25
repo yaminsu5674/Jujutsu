d56bd8058158dbe92d38f9d84d7b33f705012b2a
kim minsu <yaminsu5674@gmail.com>
2026-01-25 15:22:53 +0900

    feat: create Character Ability Tags

--- Source/Jujutsu/Private/JujutsuGameplayTags.cpp
+++ Source/Jujutsu/Private/JujutsuGameplayTags.cpp
@@ -7,5 +7,13 @@
 namespace JujutsuGameplayTags
 {
 	/** Input Tags */
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move,"InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_A, "InputTag.A");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_S, "InputTag.S");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_D, "InputTag.D");
+
+	/** Character Ability Tags */
+	UE_DEFINE_GAMEPLAY_TAG(Character_Ability_A, "Character.Ability.A");
+	UE_DEFINE_GAMEPLAY_TAG(Character_Ability_S, "Character.Ability.S");
+	UE_DEFINE_GAMEPLAY_TAG(Character_Ability_D, "Character.Ability.D");
 }
