d27b337b394cdfb250a555a45b30f36429554109
kim minsu <yaminsu5674@gmail.com>
2026-01-26 14:24:12 +0900

    feat: Capture Relevant Attirubutes

--- Source/Jujutsu/Private/AbilitySystem/ExecCalc/JujutsuExecCalc_DamageTaken.cpp
+++ Source/Jujutsu/Private/AbilitySystem/ExecCalc/JujutsuExecCalc_DamageTaken.cpp
@@ -1,7 +1,29 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "AbilitySystem/ExecCalc/JujutsuExecCalc_DamageTaken.h"
+#include "AbilitySystem/JujutsuAttributeSet.h"
+#include "JujutsuGameplayTags.h"
+
+struct FJujutsuDamageCapture
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+
+	FJujutsuDamageCapture()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UJujutsuAttributeSet, AttackPower, Source, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UJujutsuAttributeSet, DefensePower, Target, false)
+	}
+};
+
+static const FJujutsuDamageCapture& GetJujutsuDamageCapture()
+{
+	static FJujutsuDamageCapture JujutsuDamageCapture;
+	return JujutsuDamageCapture;
+}
 
 UJujutsuExecCalc_DamageTaken::UJujutsuExecCalc_DamageTaken()
 {
+	RelevantAttributesToCapture.Add(GetJujutsuDamageCapture().AttackPowerDef);
+	RelevantAttributesToCapture.Add(GetJujutsuDamageCapture().DefensePowerDef);
 }
