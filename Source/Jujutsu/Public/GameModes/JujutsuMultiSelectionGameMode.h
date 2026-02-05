17849010a83de91e898d217d37ef256a86ddadff
kim minsu <yaminsu5674@gmail.com>
2026-02-05 21:10:13 +0900

    feat: create GameMode Blueprints with Multi

---
+++ Source/Jujutsu/Public/GameModes/JujutsuMultiSelectionGameMode.h
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameModes/JujutsuSelectionGameMode.h"
+#include "JujutsuMultiSelectionGameMode.generated.h"
+
+/** 선택 모드 상속. 멀티 플레이용 선택. */
+UCLASS()
+class JUJUTSU_API AJujutsuMultiSelectionGameMode : public AJujutsuSelectionGameMode
+{
+	GENERATED_BODY()
+
+};
